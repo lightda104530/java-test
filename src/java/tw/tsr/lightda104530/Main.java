bc492123064fe06b6ea2690e1c14124651a3f714
TSRlightda <lightda104530@gmail.com>
2017-03-31 13:58:01 +0800

    Hello World!! I'm lightda!!!

---
+++ src/java/tw/tsr/lightda104530/Main.java
@@ -0,0 +1,12 @@
+package tw.tsr.lightda104530;
+
+**/**Please don't touch or change anything in this file**/**
+**/**This file was make by lightda104530**/**
+
+public class Main {
+
+    public static void main(String[] args) {
+        Console.writeLine("Hello World!! I'm lightda!!!");
+    }
+   
+}
