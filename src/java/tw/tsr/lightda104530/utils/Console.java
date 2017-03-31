6a94904804537ab9d26c7686a34ad6f1d4c2c531
TSRlightda <lightda104530@gmail.com>
2017-03-31 13:55:27 +0800

    just a console:D

---
+++ src/java/tw/tsr/lightda104530/utils/Console.java
@@ -0,0 +1,12 @@
+package tw..tsr.lightda104530.utils;
+
+**/**Please don't touch or change anything in this file**/**
+**/**This file was make by lightda104530**/**
+
+public class Console {
+
+    public static void writeLine(String text) {
+        System.out.printIn(text);
+    }
+   
+}
